18ff19a1d0853f5b2d8cdeef6c86740fd63993b3
Anurag Pal <apal895@gmail.com>
2022-03-02 11:11:53 +0530

    load more projects done

--- src/pages/Design/Design.js
+++ src/pages/Design/Design.js
@@ -1,19 +1,46 @@
+import { useState, useEffect } from "react";
 import { Header } from "../../components";
 import MainLayout from "../../layouts/MainLayout";
 import styles from "./Design.module.scss";
 import uibg from "../../assets/bg/uiux-bg.jpg";
 import { Grid } from "@mui/material";
 
+const raw = [
+  {
+    title: "Project 1",
+    image: "https://source.unsplash.com/random/800x600",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    url: "https://www.google.com",
+  },
+];
+
 const Design = () => {
-  const projects = [
-    {
-      title: "Project 1",
-      image: "https://source.unsplash.com/random/800x600",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      url: "https://www.google.com",
-    },
-  ];
+  const [data, setData] = useState([]);
+  const [projects, setProjects] = useState([]);
+  const [projectsLoadedIndex, setProjectsLoadedIndex] = useState(5); // storing index so actual count is count + 1
+
+  useEffect(() => {
+    let tempData = Array(20).fill(raw[0]);
+    setData(tempData);
+    setProjects(tempData.slice(0, 6));
+  }, []);
+
+  function handleClickMore() {
+    if (projectsLoadedIndex <= data.length - 6) {
+      setProjects([
+        ...projects,
+        ...data.slice(projectsLoadedIndex, projectsLoadedIndex + 6),
+      ]);
+      setProjectsLoadedIndex(projectsLoadedIndex + 6);
+    } else if (projectsLoadedIndex <= data.length) {
+      setProjects([
+        ...projects,
+        ...data.slice(projectsLoadedIndex, data.length - 1),
+      ]);
+      setProjectsLoadedIndex(data.length - 1);
+    }
+  }
 
   return (
     <MainLayout>
@@ -51,17 +78,27 @@ const Design = () => {
             </h1>
             <div className={styles.portfolio}>
               <Grid container spacing={1}>
-                {Array(6)
-                  .fill(projects[0])
-                  .map((project, i) => (
-                    <Grid item key={i} md={4} sm={6} xs={12}>
-                      <div className={styles.project}>
-                        <img src={project.image} alt={project.title} />
-                        <span>+</span>
-                      </div>
-                    </Grid>
-                  ))}
+                {projects.map((project, i) => (
+                  <Grid item key={i} md={4} sm={6} xs={12}>
+                    <div className={styles.project}>
+                      <img src={project.image} alt={project.title} />
+                      <span>+</span>
+                    </div>
+                  </Grid>
+                ))}
               </Grid>
+              {projects.length < data.length && (
+                <p
+                  style={{
+                    textAlign: "right",
+                    fontWeight: 700,
+                    cursor: "pointer",
+                  }}
+                  onClick={handleClickMore}
+                >
+                  Load More
+                </p>
+              )}
             </div>
           </div>
         </main>
